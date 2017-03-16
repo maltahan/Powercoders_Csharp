c2aba19c0b550c78c46ef14c365f4f755fa0e592
rufer7 <marc.rufer@d-fens.net>
2017-03-16 10:57:13 +0100

    minor

--- src/Org.Powercoders.Api/DbContext/PowercodersDbContext.cs
+++ src/Org.Powercoders.Api/DbContext/PowercodersDbContext.cs
@@ -1,5 +1,5 @@
 ﻿/**
- * Copyright 2017 d-fens GmbH
+ * Copyright 2017 powercoders
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
